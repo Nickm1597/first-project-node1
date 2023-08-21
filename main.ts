ef1ab4c3ae67753dc4a99a0f7415e936a3196881
Vanessa Mele <melevanessa385@gmail.com>
2023-08-21 14:53:35 +0200

    first project

---
+++ main.ts
@@ -0,0 +1,13 @@
+console.log('First Project Node')
+console.log('This is my first project')
+
+let myVar: number = 3; // Sintassi TypeScript;
+myVar += 10; // 13
+
+console.log('myVar = ', myVar);
+
+interface myInterface {
+    myNumber: number;
+    myString: string;
+}
+
